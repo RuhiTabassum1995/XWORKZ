2a9d4f8a937263040f78b6614a5593b4f0e91f61
RuhiTabassum1995 <ruhi.tabu@gmail.com>
2021-07-25 18:00:50 +0530

    Operator.java

---
+++ Operator.java
@@ -0,0 +1,13 @@
+class Operator{
+public static void main(String[] args){
+int e=500;
+int f=600;
+int g=(++e)+f;
+System.out.println(e++);
+System.out.println(e+f);
+System.out.println((++f)+g);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+}
+}
